161e88b5fd3f3dd81753b62fa06ed755fcb47728
Evan Overman <evanrileyoverman@gmail.com>
2026-01-26 17:45:52 -0800

    update reciever to use dual rs485

--- nile-reciever/include/uart.h
+++ nile-reciever/include/uart.h
@@ -2,6 +2,10 @@
 #define UART_H
 
 #include <stdlib.h>
+#include <driver/uart.h>
+#include <hal/uart_types.h>
+#include <driver/gpio.h>
+
 
 #define UART_BUFFER_SIZE (1024 * 8)  /* 8KB */
 #define UART_QUEUE_SIZE 32
@@ -13,20 +17,35 @@
 #define UART_PIN_TX 4
 #define UART_PIN_RX 5
 
-/**
- * Initialize UART.
- */
-void uart_init(void);
+typedef struct {
+    QueueHandle_t queue;
+    uart_port_t port;
+    int pin_tx;
+    int pin_rx;
+} uart_t;
+
+typedef void (*uart_print_function_t)(const char*);
 
 /**
- * Send the given NULL terminated string down the UART serial connection.
+ * Initialize UART with a default full duplex configuration.
  */
-void uart_send(const char* msg, size_t n);
+void uart_init(uart_t* uart, uart_port_t port, gpio_num_t pin_tx, gpio_num_t pin_rx);
+
+/**
+ * Initialize UART with a default half duplex configuration intended for use as
+ * RS485.
+ */
+void uart_rs485_init(uart_t* uart, uart_port_t port, gpio_num_t pin_tx, gpio_num_t pin_rx);
+
+/**
+ * Send the given buffer of length `n`.
+ */
+void uart_send(uart_t* uart, const char* msg, size_t n);
 
 /**
  * Read not more that `n` bytes off of serial into the given buffer. Returns the
  * number of bytes read off of UART.
  */
-size_t uart_recieve(char* msg, size_t n);
+size_t uart_recieve(uart_t* uart, char* msg, size_t n);
 
 #endif  /* UART_H */
--- nile-reciever/src/main.c
+++ nile-reciever/src/main.c
@@ -2,21 +2,25 @@
 #include <esp_task_wdt.h>
 
 #include "uart.h"
+#include "rs485.h"
 
 #define BUFFER_SIZE (1024 * 8)
+
+static rs485_t rs485;
 
 static char buffer_usb[BUFFER_SIZE] = { 0 };
 static char buffer_uart[BUFFER_SIZE] = { 0 };
 
 void app_main() {
-    uart_init();
+    rs485_init(&rs485, UART_NUM_2, GPIO_NUM_4, GPIO_NUM_5, -1, -1 ,-1);
 
     while (1) {
         size_t bytes_in_usb = fread(buffer_usb, sizeof *buffer_usb, BUFFER_SIZE, stdin);
-        size_t bytes_in_uart = uart_recieve(buffer_uart, BUFFER_SIZE);
+        size_t bytes_in_uart = rs485_recieve(&rs485, buffer_uart, BUFFER_SIZE);
 
-        uart_send(buffer_usb, bytes_in_usb);
+        rs485_send(&rs485, buffer_usb, bytes_in_usb);
         fwrite(buffer_uart, sizeof *buffer_uart, bytes_in_uart, stdout);
+        
         vTaskDelay(5);
     }
 }
